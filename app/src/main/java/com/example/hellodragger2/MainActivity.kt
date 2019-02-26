91c98f1c35a410f306503111bbf9ece4b59e8a19
GuilleOrtizGb <guillermo.ortiz@globant.com>
2019-02-26 17:44:28 -0300

    first commit wip

--- app/src/main/java/com/example/hellodragger2/MainActivity.kt
+++ app/src/main/java/com/example/hellodragger2/MainActivity.kt
@@ -10,93 +10,48 @@ import dagger.Module
 import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Qualifier
+
+const val LOVE = "Love"
+const val HATE = "Hate"
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var info: Info2
+    @Inject @field:Named(LOVE)
+    lateinit var infoLove: Info
 
-//    @Inject
-//    lateinit var data: Itext
+    @Inject @field:Named(HATE)
+    lateinit var infoHate: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DaggerMagicBox.create().poke(this)
+        DaggerMagicBox.create().inject(this)
 
-        txt.setText(info.text)
-
-//-----------My implententaion -----------//
-//        var DefaultMessage = implementation()
-//        var GuilleMEssage= implementationGuille()
-//
-//        var mData= data(GuilleMEssage)
-//        txt.setText(mData.getData())
-
-
-//--------My Implementation Dagger2------------//
-//        Toast.makeText(this,kk, LENGTH_LONG)
-
-//
-//        DaggerMagicBox2.create().inject(this)
-//       DaggerMagicBox.create().inject2(GuilleMEssage)
-
-        //txt.setText(data.show())
+        txt.setText("${infoHate.text} ${infoLove.text}")
     }
 }
 
 @Module
 class Bag{
-    @Provides
+    @Provides @Named(LOVE)
     fun sayLoveDagger2(): Info{
         return Info("Love Dagger 2")
     }
-}
-@Module
-class Bag2{
-    @Provides
-    fun sayHateDagger2(): Info2{
-        return Info2("Hate Dagger 2")
+    @Provides @Named(HATE)
+    fun sayHateDagger2(): Info{
+        return Info("Hate Dagger 2")
     }
 }
 
 class Info(val text: String)
-class Info2(val text: String)
 
-@Component (modules = [Bag::class, Bag2::class])
+@Component (modules = [Bag::class])
 interface MagicBox {
     //I also need to tell my magic box, it is there to perform it’s
     // magic on MainActivity. So to do that, I create a poke function
     // accepting MainActivity in my MagicBox.
-    fun poke(app: MainActivity)//inject()
+    fun inject(app: MainActivity)//inject()
 }
-
-//-----------My Implementation------------//
-
-//@Component
-//interface MagicBox2 {
-//    fun inject(app: MainActivity)
-//}
-
-/*class data @Inject constructor (var texto: Itext){
-    fun getData(): String{
-        return texto.show()
-    }
-}
-
-interface Itext{
-    fun show(): String
-}
-
-class implementation @Inject constructor(): Itext{
-    override fun show(): String {
-       return  "Hello implementation"
-    }
-}
-
-class implementationGuille @Inject constructor(): Itext{
-    override fun show(): String {
-        return   "Hello Guille"
-    }
-}*/
